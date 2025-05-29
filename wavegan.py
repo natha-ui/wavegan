024a282a85115311d90330100e8810f6bdf39132
natha-ui <136472669+natha-ui@users.noreply.github.com>
2025-05-29 20:02:14 +0900

    Update wavegan.py

--- wavegan.py
+++ wavegan.py
@@ -2,11 +2,16 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Conv1DTranspose, Conv1D, BatchNormalization, Dense, Reshape
 from tensorflow.keras.models import Model
+
+def lrelu(inputs, alpha=0.2):
+    return tf.maximum(alpha * inputs, inputs)
+
 class WaveGANGenerator(Model):
     def __init__(self, slice_len=16384, nch=1, kernel_len=25, dim=64,
                  use_batchnorm=False, upsample='zeros'):
         super(WaveGANGenerator, self).__init__()
-        assert slice_len in [16384, 32768, 65536]
+        assert slice_len in [16384, 32768, 65536], "Invalid slice_len"
+
         self.slice_len = slice_len
         self.nch = nch
         self.kernel_len = kernel_len
@@ -17,74 +22,60 @@ class WaveGANGenerator(Model):
         dim_mul = 16 if slice_len == 16384 else 32
         self.fc = layers.Dense(16 * dim * dim_mul)
         self.reshape = layers.Reshape((16, dim * dim_mul))
-        self.bn_layers = []
 
-        def bn_layer():
-            return layers.BatchNormalization() if use_batchnorm else tf.identity
+        # BatchNorm factory
+        def bn():
+            return layers.BatchNormalization() if use_batchnorm else None
 
-        self.upconvs = []
+        # Upsampling (transposed conv) layers
+        self.upconv_layers = []
         for _ in range(4):
             dim_mul //= 2
-            self.upconvs.append((dim * dim_mul, bn_layer()))
-        if slice_len == 16384:
-            self.final_layers = [(nch, None)]
-        elif slice_len == 32768:
-            self.final_layers = [
-                (dim, bn_layer()),
-                (nch, None)
-            ]
-        elif slice_len == 65536:
-            self.final_layers = [
-                (dim, bn_layer()),
-                (nch, None)
-            ]
+            conv = layers.Conv2DTranspose(filters=dim * dim_mul,
+                                          kernel_size=(1, kernel_len),
+                                          strides=(1, 4),
+                                          padding='same')
+            self.upconv_layers.append((conv, bn()))
 
-    def conv1d_transpose(self, inputs, filters, kernel_size, stride):
-        # Use Conv2DTranspose and reshape to simulate 1D transposed convolution
+        # Final layers based on slice length
+        self.final_layers = []
+        if slice_len == 16384:
+            self.final_layers.append((layers.Conv2DTranspose(nch, (1, kernel_len), (1, 4), padding='same'), None))
+        elif slice_len == 32768:
+            self.final_layers.append((layers.Conv2DTranspose(dim, (1, kernel_len), (1, 4), padding='same'), bn()))
+            self.final_layers.append((layers.Conv2DTranspose(nch, (1, kernel_len), (1, 2), padding='same'), None))
+        elif slice_len == 65536:
+            self.final_layers.append((layers.Conv2DTranspose(dim, (1, kernel_len), (1, 4), padding='same'), bn()))
+            self.final_layers.append((layers.Conv2DTranspose(nch, (1, kernel_len), (1, 4), padding='same'), None))
+
+    def conv1d_transpose_layer(self, inputs, conv2d_layer):
         x = tf.expand_dims(inputs, axis=1)  # (B, 1, T, C)
-        x = layers.Conv2DTranspose(filters=filters,
-                                   kernel_size=(1, kernel_size),
-                                   strides=(1, stride),
-                                   padding='same')(x)
-        return x[:, 0]  # Remove dummy spatial dimension
+        x = conv2d_layer(x)
+        return x[:, 0]  # Remove dummy dimension
 
     def call(self, z, training=False):
-        dim_mul = 16 if self.slice_len == 16384 else 32
         x = self.fc(z)
         x = self.reshape(x)
         if self.use_batchnorm:
             x = layers.BatchNormalization()(x, training=training)
         x = tf.nn.relu(x)
-        dim_mul //= 2
 
-        # Main upsampling blocks
-        for filters, bn in self.upconvs:
-            x = self.conv1d_transpose(x, filters, self.kernel_len, 4)
-            if self.use_batchnorm:
+        # Upsampling blocks
+        for conv, bn in self.upconv_layers:
+            x = self.conv1d_transpose_layer(x, conv)
+            if bn is not None:
                 x = bn(x, training=training)
             x = tf.nn.relu(x)
 
-        # Final layer(s)
-        if self.slice_len == 16384:
-            filters, _ = self.final_layers[0]
-            x = self.conv1d_transpose(x, filters, self.kernel_len, 4)
-        elif self.slice_len == 32768:
-            for i, (filters, bn) in enumerate(self.final_layers):
-                stride = 4 if i == 0 else 2
-                x = self.conv1d_transpose(x, filters, self.kernel_len, stride)
-                if bn is not None:
-                    x = bn(x, training=training)
-                if i == 0:
-                    x = tf.nn.relu(x)
-        elif self.slice_len == 65536:
-            for filters, bn in self.final_layers:
-                x = self.conv1d_transpose(x, filters, self.kernel_len, 4)
-                if bn is not None:
-                    x = bn(x, training=training)
+        # Final conv layers
+        for i, (conv, bn) in enumerate(self.final_layers):
+            x = self.conv1d_transpose_layer(x, conv)
+            if bn is not None:
+                x = bn(x, training=training)
+            if i < len(self.final_layers) - 1 or self.slice_len != 16384:
                 x = tf.nn.relu(x)
+
         return tf.nn.tanh(x)
-def lrelu(inputs, alpha=0.2):
-    return tf.maximum(alpha * inputs, inputs)
 
 def apply_phaseshuffle(x, rad, pad_type='reflect'):
     b, x_len, nch = x.shape
